2a09cde04091adc087eb9d4a3ae633fd22ce3db6
Jitin Dodd <jitindodd@gmail.com>
2019-01-16 15:17:27 -0500

    Added Sign in with Google.

--- screens/Login/Login.tsx
+++ screens/Login/Login.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Facebook } from "expo";
+import { Facebook, Google } from "expo";
 import { View, Text } from "react-native";
 //@ts-ignore -- RN styled components arent' typed
 import styled from "styled-components/native";
@@ -10,6 +10,27 @@ export interface LoginProps {
 }
 
 export default class Login extends React.Component<LoginProps> {
+  logInWithGoogle = async () => {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId:
+          "901411520285-nhnhkk5t6b6c7rll4t7qi02qo7iniq19.apps.googleusercontent.com",
+        iosClientId:
+          "901411520285-taih7lofpvqajbvfpk4c81a9juj0b04b.apps.googleusercontent.com",
+        scopes: ["profile", "email"]
+      });
+
+      if (result.type === "success") {
+        this.props.navigation.navigate("App");
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  };
+
   logInWithFacebook = async () => {
     try {
       const loginResponse = await Facebook.logInWithReadPermissionsAsync(
@@ -47,16 +68,42 @@ export default class Login extends React.Component<LoginProps> {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <FacebookButton onPress={this.logInWithFacebook}>
-          <FontAwesome
-            name="facebook"
-            size={32}
-            color="white"
-            style={{ marginRight: 10 }}
-          />
-          <Text style={{ color: "white" }}>Sign in with Facebook</Text>
-        </FacebookButton>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#0A0D32"
+        }}
+      >
+        <View
+          style={{ flex: 2, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text style={{ fontSize: 35, color: "white", fontWeight: "400" }}>
+            MedCare
+          </Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          <FacebookButton onPress={this.logInWithFacebook}>
+            <FontAwesome
+              name="facebook"
+              size={32}
+              color="white"
+              style={{ marginRight: 10 }}
+            />
+            <Text style={{ color: "white" }}>Sign in with Facebook</Text>
+          </FacebookButton>
+
+          <GoogleButton onPress={this.logInWithGoogle}>
+            <FontAwesome
+              name="google"
+              size={32}
+              color="white"
+              style={{ marginRight: 10 }}
+            />
+            <Text style={{ color: "white" }}>Sign in with Google</Text>
+          </GoogleButton>
+        </View>
       </View>
     );
   }
@@ -65,6 +112,16 @@ export default class Login extends React.Component<LoginProps> {
 const FacebookButton = styled.TouchableOpacity`
   flex-direction: row;
   background-color: #3c5a99;
+  border-radius: 5px;
+  width: auto;
+  align-items: center;
+  padding: 10px;
+  margin-top: 15px;
+`;
+
+const GoogleButton = styled.TouchableOpacity`
+  flex-direction: row;
+  background-color: #db3236;
   border-radius: 5px;
   width: auto;
   align-items: center;
